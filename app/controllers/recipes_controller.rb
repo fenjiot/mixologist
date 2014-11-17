7f4849bed3ec42c720f4bff8b537e32c5fa636dc
fenjiot <fenjiot@gmail.com>
2014-11-17 16:41:28 -0500

    User can edit recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,10 +16,31 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+    @recipe = load_recipe_from_url
+  end
+
+  def edit
+    @recipe = load_recipe_from_url
+  end
+
+  def update
+    @recipe = load_recipe_from_url
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def recipe_params
     params.require(:recipe).
       permit(:name, :instructions)
+  end
+
+  def load_recipe_from_url
+    Recipe.find(params[:id])
   end
 end
