54a0755443713c4d5a047ba1d363d534f2960c9a
fenjiot <fenjiot@gmail.com>
2014-11-19 10:31:08 -0500

    User can add ingredients to recipes

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,12 +1,14 @@
 class RecipeIngredientsController < ApplicationController
-  def new
+  def create
+    recipe_ingredient = RecipeIngredient.new(recipe_ingredient_params)
+    recipe_ingredient.save
+
+    redirect_to :back
   end
 
-  def create
-    binding.pry
-    # ingredient_to_add = ingredient_of_concern
-    # recipe = load_recipe_from_url
-    # recipe.add_ingredient(ingredient_to_add)
+  def update
+    recipe_ingredient = RecipeIngredient.find(params[:id])
+    recipe_ingredient.update(recipe_ingredient_params)
 
     redirect_to :back
   end
@@ -22,15 +24,11 @@ class RecipeIngredientsController < ApplicationController
 
   def recipe_ingredient_params
     params.require(:recipe_ingredient).
-      permit(:ingredient_id)
+      permit(:ingredient_id, :qty, :uom).
+      merge(recipe_id: params[:recipe_id])
   end
 
   def load_recipe_from_url
     Recipe.find(params[:recipe_id])
-  end
-
-  def ingredient_of_concern
-    recipe_ingredient = RecipeIngredient.find_by(recipe_id: params[:recipe_id])
-    recipe_ingredient.ingredient
   end
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,20 +1,12 @@
 class RecipeIngredient < ActiveRecord::Base
-  UNIT_OF_MEASURE = %W[ ml oz dash tsp tbsp shot cup nip #{"half pint"} #{"pint (US)"} #{"pint (UK)"} tenth fifth qt gallon ]
+  UNIT_OF_MEASURE = %W[ ml oz dash tsp tbsp shot cup nip #{"half pint"}
+                      #{"pint (US)"} #{"pint (UK)"} tenth fifth qt gallon ]
 
   belongs_to :recipe
   belongs_to :ingredient
+  delegate :name, to: :ingredient
 
-  private
-
-  def valid_uom
-    unless UNIT_OF_MEASURE.include?(uom)
-      errors.add(:uom, "Unit Of Measure must be one of the predefined UOM")
-    end
+  def self.valid_uom
+    UNIT_OF_MEASURE
   end
-
-  # def ingredient_is_not_already_in_recipe
-  #   if recipe_ingredient_id == RecipeIngredient.find_by(:ingredient_id)
-  #     errors.add(:base, "Ingredient already added to recipe")
-  #   end
-  # end
 end
