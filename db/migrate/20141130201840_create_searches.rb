d662c63f47153219f1915dc957c77251b2c7f75a
fenjiot <fenjiot@gmail.com>
2014-12-01 17:03:07 -0500

    Refactor to not use scenic gem

--- db/migrate/20141130201840_create_searches.rb
+++ db/migrate/20141130201840_create_searches.rb
@@ -1,5 +1,42 @@
 class CreateSearches < ActiveRecord::Migration
-  def change
-    create_view :searches
+  def up
+    execute <<-SQL
+    CREATE VIEW searches AS
+      SELECT
+        recipes.id AS searchable_id,
+        'Recipe' AS searchable_type,
+        recipes.name AS term
+      FROM recipes
+
+      UNION
+
+      SELECT
+        recipes.id AS searchable_id,
+        'Recipe' AS searchable_type,
+        recipes.instructions AS term
+      FROM recipes
+
+      UNION
+
+      SELECT
+        ingredients.id AS searchable_id,
+        'Ingredient' AS searchable_type,
+        ingredients.name AS term
+      FROM ingredients
+
+      UNION
+
+      SELECT
+        ingredients.id AS searchable_id,
+        'Ingredient' AS searchable_type,
+        ingredients.description AS term
+      FROM ingredients
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+    DROP VIEW searches
+    SQL
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -67,25 +67,4 @@ ActiveRecord::Schema.define(version: 20141130215417) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
-        create_view :searches, sql_definition:<<-SQL
-          SELECT recipes.id AS searchable_id,
-    'Recipe'::text AS searchable_type,
-    recipes.name AS term
-   FROM recipes
-UNION
- SELECT recipes.id AS searchable_id,
-    'Recipe'::text AS searchable_type,
-    recipes.instructions AS term
-   FROM recipes
-UNION
- SELECT ingredients.id AS searchable_id,
-    'Ingredient'::text AS searchable_type,
-    ingredients.name AS term
-   FROM ingredients
-UNION
- SELECT ingredients.id AS searchable_id,
-    'Ingredient'::text AS searchable_type,
-    ingredients.description AS term
-   FROM ingredients;
-        SQL
 end
