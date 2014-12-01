d9608e4ee0f23c373d30cc74d659d43112035f94
fenjiot <fenjiot@gmail.com>
2014-12-01 09:33:10 -0500

    Refactor 2

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20141130215417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_trgm"
 
   create_table "ingredients", force: true do |t|
     t.string   "name",        null: false
