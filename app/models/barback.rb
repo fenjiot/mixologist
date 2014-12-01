1d61b42144525dedf6f635f22d75c02ae8660dc7
fenjiot <fenjiot@gmail.com>
2014-12-01 16:48:23 -0500

    Prettify

---
+++ app/models/barback.rb
@@ -0,0 +1,11 @@
+class Barback
+  # def lookup(search)
+  #   result = Recipe.basic_search(search) ||
+  #     Ingredient.basic_search(search)
+  #   binding.pry
+  #   result
+  # end
+  #
+  # private
+  #
+end
