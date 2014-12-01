5451d14ee2013b37834d11a6b1438ba64d95cb00
fenjiot <fenjiot@gmail.com>
2014-12-01 16:48:23 -0500

    Prettify

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,5 @@
 class Rating < ActiveRecord::Base
-  VALUES = [[1, "1"], [2, "2"], [3, "Meh"], [4, "4"], [5, "5"]]
+  VALUES = [[1, "."], [2, "."], [3, "."], [4, "."], [5, "."]]
 
   belongs_to :rater, class_name: "User"
   belongs_to :rated_recipe, class_name: "Recipe"
