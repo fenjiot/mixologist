720d6c558da00d726dce7750cd7ae34e2a42f55a
fenjiot <fenjiot@gmail.com>
2014-12-03 17:06:02 -0500

    Refactor search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,22 +1,10 @@
 class SearchesController < ApplicationController
   skip_before_action :require_login, only: [:index]
   def index
-    raw_results = Search.new(query).results
-    @results = processed_results(raw_results)
+    @results = Search.new(query).results
   end
 
   private
-
-  def processed_results(raw_results)
-    results = []
-    grouped_results = raw_results.group_by(&:class)
-    grouped_results.each do |_key, group|
-      group.each do |item|
-        results << item
-      end
-    end
-    results
-  end
 
   def query
     params[:search]
