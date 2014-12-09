56368d0885333060553048b94f7a3207f2e9fe4d
fenjiot <fenjiot@gmail.com>
2014-12-09 14:48:08 -0500

    Refactor

--- app/assets/javascripts/for-select2.js
+++ app/assets/javascripts/for-select2.js
@@ -1,5 +1,5 @@
-$(function(){ $("select#recipe_ingredient_unit_of_measure").select2(); });
-
-$(function(){ $("select#recipe_ingredient_ingredient_id").select2(); });
-
-$(function(){ $("select#list_recipe_recipe_id").select2(); });
+$(function(){
+  $("select#recipe_ingredient_unit_of_measure").select2();
+  $("select#recipe_ingredient_ingredient_id").select2();
+  $("select#list_recipe_recipe_id").select2();
+});
